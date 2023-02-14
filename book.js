6f90d6f2627775c526dbaef2f4f1bd65518df451
Muhammed Berisso <muhammedb8826@gmail.com>
2023-02-14 20:31:00 +0300

    Fix linters error

--- book.js
+++ book.js
@@ -9,50 +9,50 @@ const lists = document.querySelector('.lists');
 const bookCollection = [];
 
 function removeBooks() {
-    const el = document.querySelector(`.${this.id}`);
-    el.remove();
+  const el = document.querySelector(`.${this.id}`);
+  el.remove();
 }
 
 function createBook() {
-    let listOfBooks = [];
+  let listOfBooks = [];
 
-    if (localStorage.getItem('bookCollection') !== null) {
-        listOfBooks = JSON.parse(localStorage.getItem('bookCollection'));
-    }
-    const wrapper = document.createElement('div');
-    const p1 = document.createElement('p');
-    const p2 = document.createElement('p');
-    const line = document.createElement('hr');
-    const removeBtn = document.createElement('button');
-    wrapper.className = 'container';
-    p1.setAttribute('id', 'booktitle');
-    p2.setAttribute('id', 'bookauthor');
-    removeBtn.textContent = 'Remove';
-    for (let i = 0; i < listOfBooks.length; i += 1) {
-        p1.textContent = listOfBooks[i].title;
-        p2.textContent = listOfBooks[i].author;
-        wrapper.className = `btn-${i}`;
-        removeBtn.setAttribute('id', `btn-${i}`);
-    }
-    wrapper.append(p1, p2, removeBtn, line);
-    removeBtn.addEventListener('click', removeBooks);
+  if (localStorage.getItem('bookCollection') !== null) {
+    listOfBooks = JSON.parse(localStorage.getItem('bookCollection'));
+  }
+  const wrapper = document.createElement('div');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const line = document.createElement('hr');
+  const removeBtn = document.createElement('button');
+  wrapper.className = 'container';
+  p1.setAttribute('id', 'booktitle');
+  p2.setAttribute('id', 'bookauthor');
+  removeBtn.textContent = 'Remove';
+  for (let i = 0; i < listOfBooks.length; i += 1) {
+    p1.textContent = listOfBooks[i].title;
+    p2.textContent = listOfBooks[i].author;
+    wrapper.className = `btn-${i}`;
+    removeBtn.setAttribute('id', `btn-${i}`);
+  }
+  wrapper.append(p1, p2, removeBtn, line);
+  removeBtn.addEventListener('click', removeBooks);
 
-    lists.prepend(wrapper);
+  lists.prepend(wrapper);
 }
 
 function addingBook() {
-    const book = { title: '', author: '' };
-    book.title = bookTitle.value;
-    book.author = bookAuthor.value;
-    bookCollection.push(book);
-    if (localStorage.getItem('bookCollection') === null) {
-        localStorage.setItem('bookCollection', '[]');
-    }
+  const book = { title: '', author: '' };
+  book.title = bookTitle.value;
+  book.author = bookAuthor.value;
+  bookCollection.push(book);
+  if (localStorage.getItem('bookCollection') === null) {
+    localStorage.setItem('bookCollection', '[]');
+  }
 
-    localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-    bookAuthor.value = '';
-    bookTitle.value = '';
-    createBook();
+  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+  bookAuthor.value = '';
+  bookTitle.value = '';
+  createBook();
 }
 
 // ─── Listerners ──────────────────────────────────────────────────────────────
