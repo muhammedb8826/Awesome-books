f80e403e2426a9f5e312acd9a44e378f4a8a0a80
cosmos hagan <cosmoshagan@cosmoss-MacBook-Pro.local>
2023-02-14 10:04:29 +0000

    Add linters files and fix linter error

--- book.js
+++ book.js
@@ -1,55 +1,38 @@
 // ─── Selectors ───────────────────────────────────────────────────────────────
-const bookTitle = document.querySelector("#bookTitle");
-const bookAuthor = document.querySelector("#bookAuthor");
-const addBook = document.querySelector("#addBook");
-const lists = document.querySelector(".lists");
+const bookTitle = document.querySelector('#bookTitle');
+const bookAuthor = document.querySelector('#bookAuthor');
+const addBook = document.querySelector('#addBook');
+const lists = document.querySelector('.lists');
+
 // ─── Functions ───────────────────────────────────────────────────────────────
 
 const bookCollection = [];
-function addingBook() {
-  const book = { title: "", author: "" };
-  book.title = bookTitle.value;
-  book.author = bookAuthor.value;
-  bookCollection.push(book);
-  if (localStorage.getItem("bookCollection") === null) {
-    localStorage.setItem("bookCollection", "[]");
-  }
-  
-  localStorage.setItem("bookCollection", JSON.stringify(bookCollection));
-  bookAuthor.value = "";
-  bookTitle.value = "";
-  createBook();
+
+function removeBooks() {
+  const el = document.querySelector(`.${this.id}`);
+  el.remove();
 }
-// ─── Listerners ──────────────────────────────────────────────────────────────
-
-addBook.addEventListener("click", addingBook);
-
-function removeBooks(){
-   const el = document.querySelector(`.${this.id}`);
-   el.remove();
-}
-
 
 function createBook() {
   let listOfBooks = [];
 
-  if (localStorage.getItem("bookCollection") !== null) {
-    listOfBooks = JSON.parse(localStorage.getItem("bookCollection"));
+  if (localStorage.getItem('bookCollection') !== null) {
+    listOfBooks = JSON.parse(localStorage.getItem('bookCollection'));
   }
-  const wrapper = document.createElement("div");
-  const p1 = document.createElement("p");
-  const p2 = document.createElement("p");
-  const line = document.createElement("hr");
-  const removeBtn = document.createElement("button");
-  wrapper.className='container';
-  p1.setAttribute("id", "booktitle");
-  p2.setAttribute("id", "bookauthor");
-  removeBtn.textContent = "Remove";
-  for(let i = 0; i < listOfBooks.length; i +=1){
+  const wrapper = document.createElement('div');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const line = document.createElement('hr');
+  const removeBtn = document.createElement('button');
+  wrapper.className = 'container';
+  p1.setAttribute('id', 'booktitle');
+  p2.setAttribute('id', 'bookauthor');
+  removeBtn.textContent = 'Remove';
+  for (let i = 0; i < listOfBooks.length; i += 1) {
     p1.textContent = listOfBooks[i].title;
     p2.textContent = listOfBooks[i].author;
     wrapper.className = `btn-${i}`;
-    removeBtn.setAttribute('id',`btn-${i}`);
+    removeBtn.setAttribute('id', `btn-${i}`);
   }
   wrapper.append(p1, p2, removeBtn, line);
   removeBtn.addEventListener('click', removeBooks);
@@ -57,4 +40,21 @@ function createBook() {
   lists.prepend(wrapper);
 }
 
+function addingBook() {
+  const book = { title: '', author: '' };
+  book.title = bookTitle.value;
+  book.author = bookAuthor.value;
+  bookCollection.push(book);
+  if (localStorage.getItem('bookCollection') === null) {
+    localStorage.setItem('bookCollection', '[]');
+  }
 
+  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+  bookAuthor.value = '';
+  bookTitle.value = '';
+  createBook();
+}
+
+// ─── Listerners ──────────────────────────────────────────────────────────────
+
+addBook.addEventListener('click', addingBook);
