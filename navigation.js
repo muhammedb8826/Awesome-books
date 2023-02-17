5212662b31b2f0cb54cf94f20390c8294dabaab6
cosmosha <cosmoshagan@gmail.com>
2023-02-17 19:22:11 +0200

    Add navigation.js file

---
+++ navigation.js
@@ -0,0 +1,21 @@
+const addBookLink = document.getElementById('addBookLink');
+const listLink = document.getElementById('listLink');
+const contactLink = document.getElementById('contactLink');
+
+const bookCollections = document.getElementById('bookCollections');
+const addBooks = document.getElementById('AddBooks');
+const contactInfo = document.getElementById('contactInfo');
+
+
+listLink.addEventListener('click', () => {
+    // bookCollections.classList.toggle('active');
+    alert('hey');
+})
+
+addBookLink.addEventListener('click', () => {
+    // addBooks.classList.toggle('active');
+});
+
+contactLink.addEventListener('click', () => {
+    // contactInfo..classList.toggle('active');
+});
